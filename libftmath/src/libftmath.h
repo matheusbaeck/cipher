615a18a63f3c100907531222151fc608b33c8aca
phantom <matheusbaeck@gmail.com>
2023-11-08 02:56:08 +0100

    libftmath add Makefile and .h

---
+++ libftmath/src/libftmath.h
@@ -0,0 +1,11 @@
+#ifndef LIBFTMATH_H
+#define LIBFTMATH_H
+
+# include <stddef.h>
+
+// Function prototypes
+
+long long unsigned int	ft_pow(long long unsigned int n, size_t p);
+
+#endif // LIBFTMATH_H
+
